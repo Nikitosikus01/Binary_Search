f819c1f5da378b2d3c2109cfa3d5cad5e7fd714b
Nikita Mishkuro <Nikitosikus01@gmail.com>
2021-08-12 12:35:08 +0300

    Добавьте файлы проекта.

---
+++ Бинарный поиск.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int SearchForMiddle(int from, int to)
+{
+	return (to + from) / 2;
+}
+
+int main()
+{
+	setlocale(0, "ru");
+	int from; int to; int middle; int choice;
+	while (true)
+	{
+		cout << "Введите промежуток" << endl;
+		cout << "От "; cin >> from; 
+		cout << "До "; cin >> to; 
+		if (to > from)
+		{
+			break;
+		}
+		else
+		{
+			cout << "Промежуток нужно указывать от маленького к большому!" << endl;
+		}
+	}
+
+	cout << "Загадайте число из этого промежутка." << endl;
+	middle = SearchForMiddle(from, to);
+	cout << "\tЭто число " << middle << "?" << endl;
+	int a;
+	for (int i = 0; true; i++)
+	{
+		cout << "\t1-yes\t2-more\t3-less\t4-start over\n>>> ";
+		cin >> choice;
+		switch (choice)
+		{
+			case 1:
+			cout << "Вы загадали число " << middle << endl;
+			break;
+
+			case 2:
+			from = middle; // закидываем в from знач middle
+			middle = SearchForMiddle(from, to); //в middle сохраняется новое среднее значение 
+			cout << "\tЭто число " << middle << "?" << endl;
+			break;
+
+			case 3:
+			to = middle;
+			middle = SearchForMiddle(from, to);
+			cout << "\tЭто число " << middle << "?" << endl;
+			break;
+
+			case 4:
+			cout << "Заново." << endl;
+			break;
+
+			default:
+			cout << "Нет такого варианта. Попробуйте ещё раз." << endl;
+			i = 0;
+			break;
+		}
+		if (choice == 1)
+		{
+			a = i+1;
+			break;
+		}
+		else if (choice == 4)
+		{
+			i = 0;
+		}
+	}
+
+	cout << "Чтобы найти загаданное число понадобилось попыток: " << a << endl;
+}
+
